ad1c589412f2404a227233e4b302a7e694cb0001
asib ahmed <ahmedasib1@gmail.com>
2025-01-05 12:38:31 +0600

    feat: optional parameter and default parameter

--- output/function.js
+++ output/function.js
@@ -5,7 +5,15 @@ myFunc = () => {
     // void means no return type
     // undefined means return type is undefined. its a valid return type. a value can be undefined
 };
-let sumCalculate = (a, b) => {
+const sumCalculate = (a, b) => {
     const sum = parseInt(a) + parseInt(b); // no need to define sum return type. it will automatically detect
     return sum;
+};
+const otherFunc = (a, b, c) => {
+    console.log(a + b);
+    console.log(c);
+};
+const otherFunc2 = (a, b, c = 2) => {
+    console.log(a + b);
+    console.log(c);
 };
--- src/function.ts
+++ src/function.ts
@@ -5,7 +5,17 @@ myFunc = () => {
     // undefined means return type is undefined. its a valid return type. a value can be undefined
 }
 
-let sumCalculate = (a: string , b: string) : number => {
+const sumCalculate = (a: string , b: string) : number => {
     const sum = parseInt(a) + parseInt(b); // no need to define sum return type. it will automatically detect
     return sum;
+}
+
+const otherFunc = (a: number, b: number, c? : number) : void => { // c is optional parameter
+    console.log(a + b);
+    console.log(c);
+}
+
+const otherFunc2 = (a: number, b: number, c : number = 2) : void => { // c is default parameter
+    console.log(a + b);
+    console.log(c);
 }
