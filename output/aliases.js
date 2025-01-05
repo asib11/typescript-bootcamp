88ab7fa18767d9b9b553aa5616410d7496570920
asib ahmed <ahmedasib1@gmail.com>
2025-01-05 13:13:04 +0600

    feat: function signature

--- output/aliases.js
+++ output/aliases.js
@@ -3,3 +3,17 @@ const userDetails = (id, user) => {
     return (`user id is ${id}, name is ${user.name} and age is ${user.age}`);
 };
 console.log(userDetails(1, { name: 'John', age: 20 })); // Output: user id is 1, name is John and age is 20
+// function signature
+let calc; // calc is a alias defined for function signature
+calc = (numOne, numTwo, action) => {
+    if (action === 'add') {
+        return numOne + numTwo;
+    }
+    else {
+        return numOne - numTwo;
+    }
+};
+console.log(calc(5, 10, 'add')); // Output: 15
+const userInformation = (id, user) => {
+    return (`user id is ${id}, name is ${user.name} and age is ${user.age}`);
+};
--- src/aliases.ts
+++ src/aliases.ts
@@ -6,3 +6,25 @@ const userDetails= (id: stringOrNum, user: userType ) : string  => {
 }
 
 console.log(userDetails(1, {name: 'John', age: 20})); // Output: user id is 1, name is John and age is 20
+
+// function signature
+
+let calc: (a: number, b: number, c: string) => number; // calc is a alias defined for function signature
+
+calc = (numOne: number, numTwo: number, action: string) => {
+    if(action === 'add'){
+        return numOne + numTwo;
+    } else {
+        return numOne - numTwo;
+    }
+}
+
+console.log(calc(5, 10, 'add')); // Output: 15
+
+// Example 2
+type stringOrNumber = string | number;
+type userDetail = {name: string, age: number};
+
+const userInformation = (id: stringOrNumber, user: userDetail) : string => {
+    return (`user id is ${id}, name is ${user.name} and age is ${user.age}`);
+}
